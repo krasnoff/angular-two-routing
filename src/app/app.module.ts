9c87ca88416eee8addae32195aae671048e01b93
krasnof.kobi@gmail.com <krasnoff.kobi@gmail.com>
2017-04-11 19:48:26 +0300

    add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,26 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+const routes: Routes = [
+  { path: '', component: Page1Component },
+  { path: 'page2', component: Page2Component }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
